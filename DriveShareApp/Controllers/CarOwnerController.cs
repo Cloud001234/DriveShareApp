a91f38dd88ec13e44268ad3339d9dc48c15429f9
Cloud_1234 <cloudstoragemslm@gmail.com>
2023-05-22 22:52:31 +0300

    carownercontroller

--- DriveShareApp/Controllers/CarOwnerController.cs
+++ DriveShareApp/Controllers/CarOwnerController.cs
@@ -1,4 +1,5 @@
 ﻿using DriveShareApp.Core.Data;
+using DriveShareApp.Core.DTOs;
 using DriveShareApp.Core.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,101 @@ namespace DriveShareApp.Controllers
         public List<Tripgp> getAllTrip()
         {
             return carOwnerService.getAllTrip();
+        }
+
+        [HttpGet("getallpassenger")]
+        public List<Passengergp> getAllPassenger()
+        {
+            return carOwnerService.getAllPassenger();
+        }
+
+        [HttpPost("getpassengerbyid")]
+        public Passengergp getPassengerById(Passengergp passengergp)
+        {
+            return carOwnerService.getPassengerById(passengergp);
+        }
+
+        [HttpPost("createtrip")]
+        public void createTrip(Tripgp tripgp)
+        {
+            carOwnerService.createTrip(tripgp);
+        }
+
+        [HttpPut("updatetrip")]
+        public void updateTrip(Tripgp tripgp)
+        {
+            carOwnerService.updateTrip(tripgp);
+        }
+
+        [HttpDelete("deletetrip")]
+        public void deleteTrip(int id)
+        {
+            carOwnerService.deleteTrip(id);
+        }
+
+        [HttpPost("gettripbyid")]
+        public Tripgp getTripById(Tripgp tripgp)
+        {
+            return carOwnerService.getTripById(tripgp);
+        }
+
+        [HttpPut("activetrip")]
+        public void activeTrip(Tripgp tripgp)
+        {
+            carOwnerService.activeTrip(tripgp);
+        }
+
+        [HttpPost("checkcarowner")]
+        public void checkCarOwner(Passengergp passengergp)
+        {
+            carOwnerService.checkCarOwner(passengergp);
+        }
+
+        [HttpPost("activecarowner")]
+        public void activeCarOwner(CarOwnerDTO carOwnerDTO)
+        {
+            carOwnerService.activeCarOwner(carOwnerDTO);
+        }
+        
+        [HttpPut("updatecar")]
+        public void updateCar(CarOwnerDTO carOwnerDTO)
+        {
+            carOwnerService.updateCar(carOwnerDTO);
+        }
+        [HttpDelete("deletecar")]
+        public void deleteCar(int id)
+        {
+            carOwnerService.deleteCar(id);
+        }
+
+        [HttpPost("gettallrequest")]
+        public List<TripPasengerDTO> getAllRequest(TripPasengerDTO tripPasengerDTO)
+        {
+            return carOwnerService.getAllRequest(tripPasengerDTO);
+        }
+
+        [HttpPost("gettallaccept")]
+        public List<TripPasengerDTO> getAllAccept(TripPasengerDTO tripPasengerDTO)
+        {
+            return carOwnerService.getAllAccept(tripPasengerDTO);
+        }
+
+        [HttpPost("acceptpassenger")]
+        public void acceptPassenger(Trippassengergp trippassengergp)
+        {
+            carOwnerService.acceptPassenger(trippassengergp);
+        }
+
+        [HttpPost("getrequestbyid")]
+        public TripPasengerDTO getRequestById(TripPasengerDTO tripPasengerDTO)
+        {
+            return carOwnerService.getRequestById(tripPasengerDTO);
+        }
+
+        [HttpPost("gettripscarowner")]
+        public List<Tripgp> getTripCarOwner(Tripgp tripgp)
+        {
+            return carOwnerService.getTripCarOwner(tripgp);
         }
     }
 }
